20d75c05d4743468efb14092df8099aa6080616f
Ashutosh Panda <ashu.juga99@gmail.com>
2019-12-26 12:15:05 +0530

    Routes Declared

--- server.js
+++ server.js
@@ -7,6 +7,34 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  // res.send('hello from express');
+  // res.json({ name: 'Ashutosh' });
+  res.status(200).json({ success: true, msg: 'Show all Bootcamp' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Display a bootcamp ${req.params.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create Bootcamp' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update a bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete a bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5003;
 
 app.listen(PORT, () =>
