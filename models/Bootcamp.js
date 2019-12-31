db7fe74ad16b8e72e28aa4a697a5afebebdc3ba3
Ashutosh Panda <ashu.juga99@gmail.com>
2019-12-31 12:31:03 +0530

    Slug Generated

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+
 const BootcampSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -101,6 +103,13 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+//Create Bootcamp slug from the name
+
+BootcampSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
